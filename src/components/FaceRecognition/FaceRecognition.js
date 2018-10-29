99abe030a6d4295d8bde624f98622d777ca88c88
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-29 20:25:35 +0100

    impemaing clarifai API with face detectiom module

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const FaceRecognition = ({ onInputChange, onBtnSubmit }) =>{
+const FaceRecognition = ({ imageUrl }) =>{
     return(
-    <div className='center'>
-        <img alt='' src='https://samples.clarifai.com/face-det.jpg'></img>
-        
+    <div className='center ma'>
+        <div className='absolute mt2'>
+            <img alt='' src={imageUrl} width='500px' height='auto'></img>
+        </div>
     </div>
     )
 }
