3ef2f607be4d99856f69c1a583723f0a866c7486
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-29 19:12:53 +0100

    add Clarifia API

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImagelinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '9ee01e84d3c14779a474c498a212fe31'
+});
 
 const particlesOptions = {
   particles: {
@@ -29,8 +36,20 @@ class App extends Component {
     console.log(event.target.value);
   }
 
+
   onBtnSubmit = () =>{
     console.log('click');
+    app.models.predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg")
+      .then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+  );
   }
 
 
@@ -46,9 +65,8 @@ class App extends Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange}
           onBtnSubmit={this.onBtnSubmit}
-            
           />
-        {/* <FaceRecognition /> */} 
+        <FaceRecognition /> 
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FaceRecognition = ({ onInputChange, onBtnSubmit }) =>{
+    return(
+    <div className='center'>
+        <img alt='' src='https://samples.clarifai.com/face-det.jpg'></img>
+        
+    </div>
+    )
+}
+
+export default FaceRecognition;
