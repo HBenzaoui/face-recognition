27b3941a998651f35591436826acafd7e6e8cfbf
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-29 18:20:35 +0100

    add onChange for the input and onBtnClick for the detect btn

--- src/App.js
+++ src/App.js
@@ -18,6 +18,22 @@ const particlesOptions = {
   }
 } 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  onBtnSubmit = () =>{
+    console.log('click');
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -27,7 +43,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onBtnSubmit={this.onBtnSubmit}
+            
+          />
         {/* <FaceRecognition /> */} 
       </div>
     );
