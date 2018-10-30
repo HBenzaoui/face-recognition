89784c4724d93b8f363594b80e29b684e28ee539
Hamza Benzaoui <hamzapdd@gmail.com>
2018-10-30 18:33:35 +0100

    adding the box in the picture with boundig-box css

--- src/App.js
+++ src/App.js
@@ -30,7 +30,26 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceDetection = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) =>{
@@ -43,14 +62,8 @@ class App extends Component {
     app.models.predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-      },
-      function(err) {
-        // there was an error
-      }
-  );
+      .then(response => this.displayFaceBox(this.calculateFaceDetection(response)))
+      .catch(error => console.log(error));
   }
 
 
@@ -67,7 +80,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onBtnSubmit={this.onBtnSubmit}
           />
-        <FaceRecognition imageUrl={this.state.imageUrl}/> 
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/> 
       </div>
     );
   }
