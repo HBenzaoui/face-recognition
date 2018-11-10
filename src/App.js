7b96ab5194417c67a580f36c42b91ac5b1a139e5
Hamza Benzaoui <hamzapdd@gmail.com>
2018-11-10 15:33:18 +0100

    move API to server

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
-import Clarifai from 'clarifai';
 
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '9ee01e84d3c14779a474c498a212fe31'
-});
+// const app = new Clarifai.App({
+//   apiKey: '9ee01e84d3c14779a474c498a212fe31'
+// });
 
 const particlesOptions = {
   particles: {
@@ -85,9 +84,14 @@ class App extends Component {
 
   onBtnSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+      fetch('http://localhost:3000/imageUrl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            input: this.state.input,
+        })
+      })
+      .then(response => response.json()) 
       .then(response => {
         if(response){
           fetch('http://localhost:3000/image', {
